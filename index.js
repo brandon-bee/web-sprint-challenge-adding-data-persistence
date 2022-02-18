b44a946c670930cd7116dd0ba8db8cf06095a5d5
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-17 17:24:42 -0800

    setup connections and server running

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require('./api/server')
+
+const port = process.env.PORT || 8080
+
+server.listen(port, () => {
+  return console.log(`Listening on port ${port}...`)
+})
